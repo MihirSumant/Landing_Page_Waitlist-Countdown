303316fa7c85c17937c1d757ec889924c3717c4f
MihirSumant <mihirsumant@gmail.com>
2024-05-31 01:22:05 +0530

    Added functionality to check inputs and button click

--- js/app.js
+++ js/app.js
@@ -1,3 +1,11 @@
+let nameInput = document.getElementsByClassName("name-input")[0];
+
+let emailInput = document.getElementsByClassName("email-input")[0];
+
+let joinButton = document.getElementsByClassName("join-button")[0];
+
+let invalidEmail = document.getElementsByClassName("invalid-email")[0];
+
 const countdown = () => {
   // getting the current time
   const theTime = new Date();
@@ -16,3 +24,44 @@ const countdown = () => {
 
 countdown();
 setInterval(countdown, 1000);
+
+function emailIsValid(email) {
+  console.log(email);
+  console.log(/\S+@\S+\.\S+/.test(email));
+  return /\S+@\S+\.\S+/.test(email);
+}
+
+let addErrors = () => {
+  emailInput.classList.add("error-highlight");
+  invalidEmail.classList.add("invalid-email-show");
+};
+
+let removeErrors = () => {
+  emailInput.classList.remove("error-highlight");
+  invalidEmail.classList.remove("invalid-email-show");
+};
+
+nameInput.addEventListener("click", () => {
+  nameInput.classList.remove("error-highlight");
+});
+
+emailInput.addEventListener("click", () => {
+  removeErrors();
+});
+
+joinButton.addEventListener("click", () => {
+  if (emailIsValid(emailInput.value)) {
+    if (nameInput.value !== "") {
+      removeErrors();
+      console.log(nameInput.value);
+      joinButton.innerHTML = "Done! See you at the workshop!";
+      joinButton.classList.add("button-disabled");
+      joinButton.classList.remove("hover-effect");
+      joinButton.setAttribute("disabled", false);
+    } else {
+      nameInput.classList.add("error-highlight");
+    }
+  } else {
+    addErrors();
+  }
+});
